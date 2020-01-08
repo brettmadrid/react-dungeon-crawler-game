82330de8fcf1643e2cd6f3192218522a266be860
brettmadrid <brettmadrid9@gmail.com>
2020-01-07 22:33:50 -0800

    positioned player in world in top left corner

--- src/features/player/index.js
+++ src/features/player/index.js
@@ -6,10 +6,10 @@ function Player(props) {
   return (
     <div
       style={{
-        position: "relative",
+        position: "absolute",
         top: props.position[1],
         left: props.position[0],
-        backgroundImage: `url('${walkSprite})`,
+        backgroundImage: `url('${walkSprite}')`,
         backgroundPosition: "0 0",
         width: "40px",
         height: "40px"
